0f946fcc94d6722a86945d8d53d4c91625546ce6
Wulian <1055917385@qq.com>
2024-08-28 16:21:03 +0800

    fix

--- __main__.py
+++ __main__.py
@@ -4,14 +4,13 @@ if __name__ == "__main__":
     from feedtheforge.const import lang
     from feedtheforge.main import main
     from feedtheforge.utils import pause
+    from colorama import Fore, just_fix_windows_console
+
+    just_fix_windows_console()
 
     py_version = sys.version_info
 
     if not (3, 8) < py_version < (3, 15):
-        from colorama import Fore, just_fix_windows_console
-
-        just_fix_windows_console()
-        
         print(Fore.RED + lang.t("feedtheforge.main.unsupported_version", 
                        cur=f"{py_version.major}.{py_version.minor}.{py_version.micro}"))
         pause()
